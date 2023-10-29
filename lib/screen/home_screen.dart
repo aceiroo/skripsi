ee3861eaf1b8d84568ddd22d060c19af739c3d45
Ida Bagus Dwi Putra Purnawa <putrapurnawa@gmail.com>
2023-10-29 22:15:37 +0800

    dashboard feature

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -9,6 +9,8 @@ import 'package:stock_app/screen/detail_order_screen.dart';
 import 'package:stock_app/screen/detail_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:stock_app/screen/login_screen.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:stock_app/model/ChartData.dart';
 
 final _auth = FirebaseAuth.instance;
 var loggedInUser = _auth.currentUser;
@@ -23,17 +25,11 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int selectedIndex = 0;
+  String? totalRevenue;
+  int? totalOrder;
+  int? totalItem;
 
-  List<Widget> widgetOptions = [
-    ItemList(),
-    OrderList(),
-    Text("Dashboard"),
-  ];
-  List<Text> appBarTitle = [
-    Text("List of Items"),
-    Text("List of Orders"),
-    Text("Dashboard"),
-  ];
+  List<ChartData> salesData = [];
 
   Future<void> logout() async {
     await _auth.signOut();
@@ -47,9 +43,67 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  void getData() async {
+    final order =
+        await _firestore.collection("orders").orderBy("created_at").get();
+    final items = await _firestore.collection("items").get();
+    num sum = 0;
+
+    List<int> monthSales = [];
+    Map<int, int> countData = {};
+
+    for (var doc in order.docs) {
+      final price =
+          doc.data()["quantity"] * int.parse(doc.data()["price_per_unit"]);
+
+      sum = sum + price;
+
+      monthSales.add(doc.data()['created_at'].toDate().month);
+    }
+
+    monthSales.forEach((i) => countData[i] = (countData[i] ?? 0) + 1);
+
+    print(countData.toString());
+
+    setState(() {
+      totalItem = items.docs.length;
+      totalOrder = order.docs.length;
+      totalRevenue = sum.toString();
+
+      countData.forEach((key, value) {
+        if (key == DateTime.january) {
+          salesData.add(ChartData("Jan", value));
+        } else if (key == DateTime.february) {
+          salesData.add(ChartData("Feb", value));
+        } else if (key == DateTime.march) {
+          salesData.add(ChartData("Mar", value));
+        } else if (key == DateTime.april) {
+          salesData.add(ChartData("Apr", value));
+        } else if (key == DateTime.may) {
+          salesData.add(ChartData("May", value));
+        } else if (key == DateTime.june) {
+          salesData.add(ChartData("Jun", value));
+        } else if (key == DateTime.july) {
+          salesData.add(ChartData("Jul", value));
+        } else if (key == DateTime.august) {
+          salesData.add(ChartData("Aug", value));
+        } else if (key == DateTime.september) {
+          salesData.add(ChartData("Sep", value));
+        } else if (key == DateTime.october) {
+          salesData.add(ChartData("Oct", value));
+        } else if (key == DateTime.november) {
+          salesData.add(ChartData("Nov", value));
+        } else if (key == DateTime.december) {
+          salesData.add(ChartData("Dec", value));
+        }
+      });
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    getData();
   }
 
   @override
@@ -59,6 +113,22 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> widgetOptions = [
+      ItemList(),
+      OrderList(),
+      Dashboard(
+        totalRevenue: totalRevenue.toString(),
+        totalOrder: totalOrder ?? 0,
+        totalItem: totalItem ?? 0,
+        salesData: salesData,
+      ),
+    ];
+    List<Text> appBarTitle = [
+      Text("List of Items"),
+      Text("List of Orders"),
+      Text("Dashboard"),
+    ];
+
     return Scaffold(
         appBar: AppBar(
           title: appBarTitle.elementAt(selectedIndex),
@@ -157,6 +227,102 @@ class OrderList extends StatelessWidget {
           },
         );
       },
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  Dashboard({
+    required this.totalRevenue,
+    required this.totalOrder,
+    required this.totalItem,
+    required this.salesData,
+  });
+
+  late String totalRevenue;
+  late int totalOrder;
+  late int totalItem;
+  List<ChartData> salesData;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            Card(
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text("Total Revenue"),
+                    leading: Icon(Icons.monetization_on),
+                    subtitle: Text("Rp.${totalRevenue}"),
+                    subtitleTextStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Card(
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text("Total Order"),
+                          leading: Icon(Icons.shopping_cart),
+                          subtitle: Text(totalOrder.toString()),
+                          subtitleTextStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Card(
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text("Total Item"),
+                          leading: Icon(Icons.info),
+                          subtitle: Text(totalItem.toString()),
+                          subtitleTextStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            SfCartesianChart(
+              isTransposed: true,
+              title: ChartTitle(text: "Yearly Sales Analysis"),
+              primaryXAxis: CategoryAxis(),
+              series: [
+                BarSeries(
+                  dataSource: salesData,
+                  xValueMapper: (ChartData data, _) => data.x,
+                  yValueMapper: (ChartData data, _) => data.y,
+                  borderRadius: BorderRadius.circular(15.0),
+                  dataLabelSettings: DataLabelSettings(
+                    isVisible: true,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
